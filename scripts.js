98c378845221c3275186d92db473bf6339c08595
alexio42 <116265425+alexio42@users.noreply.github.com>
2023-05-27 19:34:48 +0100

    Add mouseover event

--- scripts.js
+++ scripts.js
@@ -3,6 +3,14 @@ const screen = document.querySelector('#screen');
 
 for (let i = 0; i < 256; i++) {
   let pixel = document.createElement('div');
-  pixel.classList.add('pixel16');
+  pixel.classList.add('pixel');
   screen.appendChild(pixel);
 }
+
+const pixels = document.querySelectorAll("div.pixel")
+
+pixels.forEach((div) => {
+  div.addEventListener('mouseover', () => {
+    div.classList.add('hover');
+  } )
+})
