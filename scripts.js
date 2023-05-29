99ebb2c9a472f48aa23da65150944154ab5a5d99
alexio42 <116265425+alexio42@users.noreply.github.com>
2023-05-29 12:04:44 +0100

    add button to turn on shade effect

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
+//create initial 16x16 grid
 const screen = document.querySelector('#screen');
-
-
 for (let i = 0; i < 256; i++) {
   let pixel = document.createElement('div');
   pixel.classList.add('pixel');
@@ -9,12 +8,13 @@ for (let i = 0; i < 256; i++) {
 
 const pixels = document.querySelectorAll("div.pixel");
 
+//Adds initial hover event listener
 pixels.forEach((div) => {
-  div.addEventListener('mouseover', () => {
-    div.classList.add('hover');
-  } )
+  div.addEventListener('mouseover', hover)
 });
 
+
+//functions to take user input and create new grid
 function squareSize(number) {
   return number * number;
 };
@@ -37,15 +37,48 @@ function createPixels(number) {
     screen.appendChild(pixel);
   }};
 
-function hoverEffect() {
+//function to add event listener for basic effect
+function hoverEffectBasic() {
+  let pixels = document.querySelectorAll("div.pixel");
+  pixels.forEach(div => div.addEventListener('mouseover', hover))};
+
+//turns div black when mousover
+function hover() {
+  this.style.backgroundColor = '#000000';
+}
+
+//function to incremet shade with each mouse
+function hoverEffectShades() {
   let pixels = document.querySelectorAll("div.pixel");
   pixels.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-      div.classList.add('hover');
-    } )
-  })
-};
+    div.removeEventListener('mouseover', hover);
+    div.addEventListener('mouseover', opacity)})};
+//       let a = shadeIncrement(opacity)
+//       div.style.backgroundColor = `rgba(0, 0, 0, ${0.1 + a})`;
+//     } )
+//   })
+// }
 
+// const opacity = 0;
+
+// function shadeIncrement(opacity) {
+//     opacity += 0.1;
+//     return opacity;
+//   };
+
+function opacity() {
+  if (this.style.backgroundColor.match(/rgba/)) {
+    let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
+    if (currentOpacity <= 0.9) {
+        this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+        this.classList.add('gray');
+    }
+} else if (this.classList == 'pixel gray' && this.style.backgroundColor == 'rgb(0, 0, 0)') {
+    return;
+} else {
+    this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';  
+}
+}
 
 function pixelSize(number) {
   return 100 / number;
@@ -59,12 +92,17 @@ function userInput() {
   } else {
     removeElementsByClass('pixel');
     createPixels(number);
-    hoverEffect();
-  }
+  } hoverEffectBasic()
 };
 
 const sizeButton = document.querySelector('#size-button');
 
 sizeButton.addEventListener('click', () => {
   userInput();
+})
+
+const shadeEffect = document.querySelector('#shade-effect');
+
+shadeEffect.addEventListener('click', () => {
+  hoverEffectShades();
 })
